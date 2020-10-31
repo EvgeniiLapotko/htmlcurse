6e769c962bc8bb04ba037f0e8ad9123e5dd2bfa5
Evgenii <lapotko.e@yandex.ru>
2020-10-31 12:26:51 +0300

    add body

--- scripts/scriptjs.js
+++ scripts/scriptjs.js
@@ -5,4 +5,12 @@ $(function(){
     dots : true,
   });
 
+  $('.works__inner').slick({
+    infinite: true,
+  	slidesToShow: 4,
+  	slidesToScroll: 1,
+  	prevArrow: '<button class="slick-arrow slick-prev"><img src="img/works/arowleft.svg" alt=""></button>',
+  	nextArrow: '<button class="slick-arrow slick-next"><img src="img/works/arowright.svg" alt=""></button>',
+  });
+
 });
